ea37f2145a11ca04cfd77495e4153a640b314b39
kurtJames <abuelfrancis@gmail.com>
2020-01-19 01:48:28 +0800

    filter news sources result to return only required data

--- src/routes/news-routes.ts
+++ src/routes/news-routes.ts
@@ -4,18 +4,33 @@ import * as NewsAPI from 'newsapi';
 const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
 const newsRoutes = Router();
 
-newsRoutes.get('/news/:page', (req, res) => {
-  (async function() {
-    const page = req.params.page;
 
-    const newsResult = await newsapi.v2.everything({
-      q: 'sports',
+newsRoutes.get('/sources', (req, res) => {
+  (async function () {
+    const { sources } = await newsapi.v2.sources({
+      category: 'technology',
+      country: 'us'
+    });
+
+    const mapSources = sources.map(src => { return { id: src.id, name: src.name }});
+    return res.json(mapSources)
+  })();
+});
+
+newsRoutes.get('/:page/:source', (req, res) => {
+  (async function () {
+    const page = req.params.page;
+    const source = req.params.source;
+    const queryOptions = {
+      sources: source,
       from: process.env.DATE_FROM,
       to: process.env.DATE_TO,
       language: 'en',
       sortBy: 'relevancy',
       page: page
-    });
+    }
+    
+    const newsResult = await newsapi.v2.everything(queryOptions);
     return res.json(newsResult)
   })();
 });
