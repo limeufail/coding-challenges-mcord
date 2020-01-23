d834b9612d749c6427f3bb10a3467553666a0933
Francis Abuel <francis_abuel@manulife.com>
2020-01-23 09:58:09 +0800

    move coding challenge 1 & 4 to its own folder

--- jcc-4.js
+++
@@ -1,8 +0,0 @@
-function calculateValidationNumber(numbers) {
-  const arrNumbers = numbers.toString().split('').map(Number);
-  const sum = arrNumbers.reduce((total, num) => total += num);
-  return (sum >= 10) ? calculateValidationNumber(sum) : sum;   
-}
-console.log(calculateValidationNumber(444444));
-console.log(calculateValidationNumber(1234));
-console.log(calculateValidationNumber(23478998));
